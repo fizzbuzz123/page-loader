c0d98ab76c5914a0b11645657928d9b04dcaee6c
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2020-03-06 19:15:59 +0600

    fix extractResourceUrls function

--- src/utils/extract-resource-urls.js
+++ src/utils/extract-resource-urls.js
@@ -6,10 +6,13 @@ const mapping = {
   script: 'src',
 };
 
-const elementsSelector = 'img[src^="http"], script[src], link[rel="stylesheet"][href]';
+const elementsSelector = 'img[src], script[src], link[rel="stylesheet"][href]';
 const extractResourceUrls = (htmlText) => {
   const $ = cheerio.load(htmlText);
-  const elements = $(elementsSelector).toArray();
+  const elements = $(elementsSelector).toArray().filter((el) => {
+    if (el.name !== 'img') return true;
+    return !$(el).attr('src').startsWith('data:');
+  });
   const urls = elements.map((el) => $(el).attr(mapping[el.name]));
   return urls;
 };
